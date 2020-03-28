3ab76ba8991a76ee6967f3a5f743ab7ecc39207e
Mark Lodato <mark@gnar.dog>
2020-03-27 21:03:33 -0400

    Adds basic websocket server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::net::TcpListener;
+use std::thread::spawn;
+
+use tungstenite::accept;
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind("127.0.0.1:3012").unwrap();
+    for stream in server.incoming() {
+        spawn(move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+
+            loop {
+                let msg = websocket.read_message().unwrap();
+                if msg.is_binary() || msg.is_text() {
+                    websocket.write_message(msg).unwrap();
+                }
+            }
+        });
+    }
 }
